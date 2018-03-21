43179226549a08c2b343ab528115e72c1d3ff12f
profyao90 <aubinya90@gmail.com>
2018-03-21 23:31:49 +0100

    fisrt commit

--- js/pokedex.js
+++ js/pokedex.js
@@ -6,3 +6,10 @@ pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+var pokeFactory = function ($resource) {
+    return $resource('https://pokeapi.co/api/v2/pokemon/:id/', {id: '@id'},
+        {
+            'update': {method: 'PUT'},
+        });
+};
